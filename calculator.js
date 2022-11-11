2c929812329312359202b19f2768906f086f969d
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-11-11 16:07:16 +0100

    added bmi calculator

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+//jshint esversion:6 (if linter gives error)
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -9,17 +9,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 //write everytime you wanna use body-parser
 
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+	console.log(__dirname);
+	res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.post("/", function(req, res) {
+	let weight = req.body.weight;
+	let height = req.body.height;
 
-	let num1 = Number(req.body.num1);
-	let num2 = Number(req.body.num2);
-
-	let result = num1 + num2;
-	res.send("The result of the calculation is " + result);
-
+	let bmiLong = 10000 * (weight / Math.pow(height, 2));
+	let bmi = bmiLong.toFixed(1);
+	res.send("Your BMI is " + bmi);
 });
 
 
