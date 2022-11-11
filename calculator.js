f4c4608567ea8e363fd8051a2cee8c1256e930e7
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-11-11 15:27:09 +0100

    sendFile html, bodyParser

--- calculator.js
+++ calculator.js
@@ -1,12 +1,22 @@
 //jshint esversion:6
 
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+//used to parse http request
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+//write everytime you wanna use body-parser
 
 app.get("/", function(req, res) {
-	res.send("Hello World");
+	res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res) {
+	res.send("Thanks for posting that");
+	console.log(req.body.num1);
+});
+
 
 app.listen(3000, function() {
 	console.log("Server is running on port 3000");
