e1422f0c9799d7ed1ba516041a90380902fee8b9
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-11-17 17:30:26 +0100

    2nd page added

--- calculator.js
+++ calculator.js
@@ -8,8 +8,19 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 //write everytime you wanna use body-parser
 
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+})
+
 app.get("/bmicalculator", function(req, res) {
 	res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res) {
+	const num1 = parseInt(req.body.num1);
+	const num2 = parseInt(req.body.num2);
+	const sum = num1 + num2;
+	res.send("The sum of " + num1 + " and " + num2 + " is: " + sum);
 });
 
 app.post("/bmicalculator", function(req, res) {
